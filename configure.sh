71304be6047a71fc08a1c743fffe0088868f41d2
YogiSoni-lazy <59290335+YogiSoni-lazy@users.noreply.github.com>
2023-10-17 20:22:22 +0530

    Create configure.sh

---
+++ configure.sh
@@ -0,0 +1,8 @@
+sudo sed -i "s/RHT_COURSE=.*/RHT_COURSE=bfx009/" /etc/rht
+sudo sed -i "s/RHT_VERSION_LOCK=.*/RHT_VERSION_LOCK='>=8.0,<9.0'/" /etc/rht
+sudo systemctl restart dynolabs-update.service
+pip install rht-labs-bfx009==8.0.3.dev13 --extra-index-url https://pypi.apps.tools-na.prod.nextcle.com/repository/labs/simple/
+lab select bfx009
+lab --version
+source ~/.venv/labs/bin/activate
+cd .venv/labs/lib/python3.6/site-packages/bfx009/
