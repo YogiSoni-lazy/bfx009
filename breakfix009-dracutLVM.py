b0221046b38ba41c4a304c5888b1ebf932128d9c
YogiSoni-lazy <59290335+YogiSoni-lazy@users.noreply.github.com>
2023-10-17 20:56:42 +0530

    Update breakfix009-dracutLVM.py

--- breakfix009-dracutLVM.py
+++ breakfix009-dracutLVM.py
@@ -71,7 +71,66 @@ class Breakfix009Dracutlvm(Default):
         userinterface.Console(items).run_items(action="Un-done")
         
     def grade(self):
-        items = []
+        """
+        Perform evaluation steps on the system
+        """
+        items = [
+            {
+                "label": "Checking lab systems",
+                "task": labtools.check_host_reachable,
+                "hosts": _targets,
+                "fatal": True,
+            },
+            steps.run_command(
+                label="Verifying lab system " + _servera,
+                hosts=[_servera],
+                command="[ ! -z $(systemctl list-units --type target --state active | grep -o $(systemctl get-default)) ]",
+                returns="0",
+                shell=True,
+            ),
+            steps.run_command(
+                label="Verifying lab system " + _servera,
+                hosts=[_servera],
+                command="[ ! -z $(lsinitrd | grep ^lvm) ]",
+                returns="0",
+                shell=True,
+            ),
+            steps.run_command(
+                label="Verifying lab system " + _servera,
+                hosts=[_servera],
+                command="dracut -fv /tmp/test.img $(awk '/^menuentry.*{/,/^}/' /boot/grub2/grub.cfg | awk -vRS="\n}\n" -vDEFAULT="$((default+1))" 'NR==DEFAULT' | grep -o '/vmlinuz-.*' | awk '{print $1}' | cut -c 10-) &> /tmp/test.dracut ; [ ! -z $(grep -o "Including module: lvm" /tmp/test.dracut | sort -u | awk '{print $3}') ]",
+                returns="0",
+                shell=True,
+            ),
+            steps.run_command(
+                label="Verifying lab system " + _servera,
+                hosts=[_servera],
+                command="[ -z $(lvs 2>&1 | grep -o rejected | sort -u) ]",
+                returns="0",
+                shell=True,
+            ),
+            steps.run_command(
+                label="Verifying lab system " + _servera,
+                hosts=[_servera],
+                command="[ -z $(grep -Ev "^[[:space:]]*#|^$|^[[:space:]]*;" /etc/lvm/lvm.conf | grep -o volume_list) ] || [ ! -z $(grep -Ev "^[[:space:]]*#|^$|^[[:space:]]*;" /etc/lvm/lvm.conf | grep volume_list | grep -o $(lvs | awk '$1~"root"{print $2}')) ]",
+                returns="0",
+                shell=True,
+            ),
+            steps.run_command(
+                label="Verifying lab system " + _servera,
+                hosts=[_servera],
+                command="[ $(awk '/^menuentry.*{/,/^}/' /boot/grub2/grub.cfg | awk -vRS="\n}\n" -vDEFAULT="$((default+1))" 'NR==DEFAULT' | grep -o '/vmlinuz-.*' | awk '{print $1}' | cut -c 10-) == $(uname -a | awk '{print $3}') ]",
+                returns="0",
+                shell=True,
+            ),
+            steps.run_command(
+                label="Verifying lab system " + _servera,
+                hosts=[_servera],
+                command="grep "filter = [ "r|.*/|" ]" /etc/lvm/lvm.conf &>> /dev/null",
+                returns="0",
+                shell=True,
+            ),
+        ]
         ui = userinterface.Console(items)
         ui.run_items(action="Grading")
         ui.report_grade()
